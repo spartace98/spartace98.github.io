6c734b252d97431ccea31c6d3503e6d67234be14
Li Chao <lchao98@gmail.com>
2020-06-28 19:04:56 +0800

    Abtraction

---
+++ js/internal-script.js
@@ -0,0 +1,44 @@
+// Sidenav
+const sideNav = document.querySelectorAll(".sidenav");
+M.Sidenav.init(sideNav, {
+onOpenStart: true,
+});
+
+// Slider
+const slider = document.querySelectorAll(".slider");
+M.Slider.init(slider, {
+indicators: true,
+height: 800,
+transition: 500,
+interval: 6000,
+});
+
+// Scrollspy
+const scrollspy = document.querySelectorAll(".scrollspy");
+M.ScrollSpy.init(scrollspy, {});
+
+// collapsible
+const collapsible = document.querySelectorAll(".collapsible");
+M.Collapsible.init(collapsible, {});
+$("[data-click]").on("click", function (e) {
+$($(this).attr("href")).trigger("click");
+});
+
+// Modal
+const modal = document.querySelector(".modal");
+M.Modal.init(modal, {});
+
+// Material Boxed
+const materialboxed = document.querySelectorAll(".materialboxed");
+M.Materialbox.init(materialboxed, {});
+
+// Carousel
+const carousel = document.querySelectorAll(".carousel");
+M.Carousel.init(carousel, {
+fullWidth: true,
+indicators: true,
+});
+
+// Tap-target
+const tap_target = document.querySelectorAll(".tap-target");
+M.TapTarget.init(tap_target, {});
